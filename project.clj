71df856abdc7fe84b264fb1a0daaf0bef1f66d74
jv <jvhaileapps@gmail.com>
2024-05-30 22:08:11 +0300

    feat: added example project to showcase usage of the framework

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clojure-bot-kit "0.1.0-SNAPSHOT"
   :description "Clojure-Bot-Kit is an experimental framework designed to leverage Lisp’s powerful abstractions for creating Telegram bots with minimal boilerplate. This project aims to boost productivity and flexibility in bot development, providing an easy-to-use and maintainable solution for building robust Telegram bots."
   :dependencies [[org.clojure/clojure "1.11.1"]]
+  :main
   :repl-options {:init-ns clojure-bot-kit.core})
